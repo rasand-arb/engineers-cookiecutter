fbd4393ebe529d36c8d89418dd750a0bd5f634cd
Andersen, Rasmus Ravn <rasmus.andersen@cobhamsatcom.com>
2024-12-09 12:06:11 +0100

    changed hook to personal needs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,33 +1,16 @@
 help = """
 Your project has been created!
-_____________________________________________________________________________
-                            ___________ _
-  \/                    __/   .::::.-'-(/-/)
-                     _/:  .::::.-' .-'\/\_`*******            __ (_))
-        \/          /:  .::::./   -._-.  d\|                 (_))_(__))
-                     /: ("'"'/    '.  (__/||             (_))__(_))--(__))
-                      \::).-'  -._  \/ \\/\|
-              __ _ .-'`)/  '-'. . '. |  (i_O
-          .-'      \       -'      '\|
-     _ _./      .-'|       '.  (    \\                           % % %
-  .-'   :      '_  \         '-'\  /|/      @ @ @               % % % %
- /      )\_      '- )_________.-|_/^\      @ @ @@@             % %\/% %
- (   .-'   )-._-:  /        \(/\'-._ `.     @|@@@@@              ..|........
-  (   )  _//_/|:  /          `\()   `\_\     |/_@@               )'-._.-._.-
-   ( (   \()^_/)_/             )/      \\    /                  /   /
-    )  _.-\\.\(_)__._.-'-.-'-.//_.-'-.-.)\-'/._                /       
-.-.-.-'   _o\ \\\     '::'   (o_ '-.-' |__\'-.-;~ ~ ~ ~ ~ ~ ~~/   /\   
-          \ /  \\\__          )_\    .:::::::.-'\            '- - -|
-     :::''':::::^)__\:::::::::::::::::'''''''-.  \                  '- - - - 
-    :::::::  '''''''''''   ''''''''''''':::. -'\  \       C. SWANSIGER
-_____':::::_____________________________________\__\_________________________
-
-If you have not done so already, create a pyenv environment for your new 
-project with:
+If you have not done so already, create a pyenv environment for your new project with:
 
 cd {{cookiecutter.repo_name}}
-pyenv virtualenv 3.11.1 {{cookiecutter.repo_name}}
-pyenv activate {{cookiecutter.repo_name}}
+
+uv venv
+
+If you use fish
+source ./venv/bin/activate.fish
+
+If you use bash
+```source ./venv/bin/activate```
 
 Install your new project in your local environment with:
 
@@ -37,5 +20,13 @@ You will need to manually add data to .gitignore to prevent it from syncing to
 version control.
 
 Don't forget to sync to GitHub. Have fun!
+
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin {{cookiecutter.github_username}}/{{cookiecutter.repo_name}}
+git push -u origin main
+
 """
 print(help)
